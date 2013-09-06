981b87f0088c506c432e89b1d0d014c834d556bd
preetinousinfosystems <satlajnousinfosystems@gmail.com>
2013-09-06 09:26:27 +0530

    Updated File added

--- src/com/ontraport/app/tools/Common.java
+++ src/com/ontraport/app/tools/Common.java
@@ -6,7 +6,9 @@ import java.util.Properties;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Common
@@ -56,5 +58,106 @@ public class Common
         }
         return jQcondition;
     }
+    
+    
+    /**
+     * Get the text from the text box
+     * @param inputTypeWebElement
+     * @return
+     */
+    public static String getValueOfTextBox (WebElement inputTypeWebElement){
+    	return inputTypeWebElement.getAttribute("value").trim();
+    }
+    
+    
+    /**
+     * Gets the first selected option from the drop down
+     * @param selectWebElement
+     * @return
+     */
+    public static String getSelectedValueFromDropDown (WebElement selectWebElement){
+    	Select selectBox = new Select(selectWebElement);
+    	String selectedOption = selectBox.getFirstSelectedOption().getText();
+    	return selectedOption;
+    }
+    
+    
+    public static boolean verifyTwoStringsAreEqual (String s1, String s2){
+    	return s1.trim().equalsIgnoreCase(s2.trim());
+    }
+    
+    public static boolean verifyStringStartsWith (String s1, String s2){
+    	return s1.trim().startsWith(s2.trim());
+    }
+    
+  
+    
+    /**
+     * Used for asserting the values in text box
+     * @param inputTypeWebElement
+     * @param expectedValue
+     * @return
+     */
+    public static boolean verifyTextBoxValue (WebElement inputTypeWebElement, String expectedValue){
+    	boolean flag = false;
+    	try {
+    		flag = verifyTwoStringsAreEqual (getValueOfTextBox(inputTypeWebElement), expectedValue);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	return flag;
+    }
+    
+    
+    /**
+     * Used for asserting the values in text box
+     * @param inputTypeWebElement
+     * @param expectedValue
+     * @return
+     */
+    public static boolean verifySelectDropDownValue (WebElement selectWebElement, String expectedValue){
+    	boolean flag = false;
+    	try {
+    		flag = verifyTwoStringsAreEqual (getSelectedValueFromDropDown(selectWebElement), expectedValue);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	return flag;
+    }
+    
+    
+    /**
+     * Used for asserting the values in text box
+     * @param inputTypeWebElement
+     * @param expectedValue
+     * @return
+     */
+    public static boolean verifyTextBoxValueStartsWith (WebElement inputTypeWebElement, String expectedValue){
+    	boolean flag = false;
+    	try {
+    		flag = verifyStringStartsWith (getValueOfTextBox(inputTypeWebElement), expectedValue);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	return flag;
+    }
+    
+    
+    /**
+     * Used for asserting the values in text box
+     * @param inputTypeWebElement
+     * @param expectedValue
+     * @return
+     */
+    public static boolean verifySelectDropDownValueStartsWith (WebElement selectWebElement, String expectedValue){
+    	boolean flag = false;
+    	try {
+    		flag = verifyStringStartsWith (getSelectedValueFromDropDown(selectWebElement), expectedValue);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    	return flag;
+    }
+    
 
 }
