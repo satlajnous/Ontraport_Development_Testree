28b3400f34f5f7acbf4650fa0dfa1cda84e84a5b
Ranisree <ranisreevn@nousinfo.com>
2013-09-11 16:54:42 +0530

    20130911_LeadScore_340,341

---
+++ src/com/ontraport/app/tests/LeadScoring_TC0341.java
@@ -0,0 +1,77 @@
+package com.ontraport.app.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+//import com.ontraport.app.pages.Contact_ListAll;
+import com.ontraport.app.pages.Contact_Settings;
+import com.ontraport.app.pages.LeadScoring_Edit;
+import com.ontraport.app.parts.SiteMenu;
+import com.ontraport.app.tools.AbstractTest;
+import com.ontraport.app.tools.Common;
+
+public class LeadScoring_TC0341 extends AbstractTest{
+	
+
+	
+	
+	@Test
+	public void testLeadScoring_AfieldMeetsAgivenCriteriaDropdown() throws Exception {
+		LeadScoring_Edit leadScoringEditPgObj = navigateToLeadScoringPage ();
+		
+		
+		leadScoringEditPgObj = leadScoringEditPgObj.clearAllConditions();
+		leadScoringEditPgObj.clickSaveButton();
+		Common.waitForPage(driver, 60);
+		
+			
+		leadScoringEditPgObj = navigateToLeadScoringPage ();
+		leadScoringEditPgObj = leadScoringEditPgObj.clickAddNewCondition();	
+		
+		String[] placeHolders = {"Select Condition", "Select Field...", "Select Condition...", "Select..."};
+		String[] selectOptions = {"Field is this value", "SelDropdown", "Equal To","SelText"};
+		
+		leadScoringEditPgObj = leadScoringEditPgObj.selectDropDownsBasedOnConditions (placeHolders, selectOptions);
+		leadScoringEditPgObj = leadScoringEditPgObj.assignLeadScoringvalue("11");
+		leadScoringEditPgObj.clickSaveButton();
+		Common.waitForPage(driver, 60);
+			
+		leadScoringEditPgObj = navigateToLeadScoringPage ();
+		
+		boolean b = leadScoringEditPgObj.verifyTheValueInDropDownBasedOnPlaceHolder ("Select Field...", "SelDropdown");
+		boolean b1 = leadScoringEditPgObj.verifyTheValueInDropDownBasedOnPlaceHolder ("Select Condition...", "Equal To");	
+		boolean b2 = leadScoringEditPgObj.verifyTheValueInDropDownBasedOnPlaceHolder("Select...", "SelText");
+		boolean b3 = leadScoringEditPgObj.verifyScoreValue("11");
+		
+		System.out.println(b);
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		
+		
+		Assert.assertTrue ("Select Field Comparision", b);
+		Assert.assertTrue ("Condition selected", b1);
+		Assert.assertTrue ("Select Comparision", b2);
+		Assert.assertTrue ("Score value", b3);
+	}
+	
+	
+	
+	
+	public LeadScoring_Edit navigateToLeadScoringPage () throws Exception{
+		SiteMenu siteMenu = (SiteMenu) new SiteMenu().init();
+		Common.waitForPage(driver, 30);
+		siteMenu.contacts();
+		siteMenu.contact_Settings();
+		
+		Contact_Settings contactSettings = (Contact_Settings) new Contact_Settings().init();
+		Common.waitForPage(driver, 30);
+		LeadScoring_Edit leadScoringEditPgObj = contactSettings.leadScoring();
+		Common.waitForPage(driver, 30);
+		return leadScoringEditPgObj;
+	}
+	
+	
+
+
+}
