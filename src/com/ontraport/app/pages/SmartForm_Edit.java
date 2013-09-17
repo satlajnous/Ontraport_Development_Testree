9732b282d05af296f52e00bff17510d2a6a422ca
Satlaj <satlajs@nousinfo.com>
2013-09-17 13:17:37 +0530

    smart forms

--- src/com/ontraport/app/pages/SmartForm_Edit.java
+++ src/com/ontraport/app/pages/SmartForm_Edit.java
@@ -47,18 +47,90 @@ public class SmartForm_Edit extends AbstractPage {
 	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-element-options']//div[select]")
     private List<WebElement> selectParentElements;
 	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-element-options']//div[select]/label")
+    private List<WebElement> selectParentElementsLabel;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-element-options']//div[select]/div")
+    private List<WebElement> selectParentElementsDiv;
 	
 	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-element-options']//div[input]/label")
+    private List<WebElement> inputParentElementsLabel;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-element-options']//div[input]/div")
+    private List<WebElement> inputtParentElementsDiv;
+	
+	//*[@id='fe-style-selector-field-style']//div[input]/div
+	//*[@id='fe-style-selector-element-options']//div[select]/descendant::select
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-mode-design']/div[1]/div/ul/li/a[text()='Form Style']")
+    private WebElement formStyletab;
+	
+	
+	//*[@id='fe-style-selector-field-style']//div[select]/div/following-sibling::select
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-field-style']//div[select]/label")
+    private List<WebElement> selectFormStylParentElementsLabel;
+												
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-field-style']//div[select]/div")
+    private List<WebElement> selectFormStylParentElementsDiv;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-field-style']//div[input]/label")
+    private List<WebElement> inputFormStylParentElementsLabel;
+	
+	@FindBy(how = How.XPATH, using = "//*[@id='fe-style-selector-field-style']//div[input]/div")
+    private List<WebElement> inputFormStylParentElementsDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='field style when unselected']]//div[select]/div")
+    private WebElement selectFormStylFildStlyUnSelectDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='Input Text Style']]//div[select]/div")
+    private List<WebElement> selectFormStylInputTextStylSelectDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='label style']]//div[select]/div")
+    private List<WebElement> selectFormStylLabelStyleSelectDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='sub text style']]//div[select]/div")
+    private List<WebElement> selectFormStylSubTextStyleSelectDiv;
+		
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='Input Text Style']]//div[input]/div")
+    private List<WebElement> inputFormStylInputTextStylSelectDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='label style']]//div[input]/div")
+    private List<WebElement> inputFormStylLabelStyleSelectDiv;
+	
+	@FindBy(how = How.XPATH, using = "//div[@id='fe-style-selector-field-style']/div[div[text()='sub text style']]//div[input]/div")
+    private List<WebElement> inputFormStylSubTextStyleSelectDiv;
+		
+		
+	
+	
+	
+	
+	
+	//div[@id='fe-style-selector-field-style']/div[div[text()='field spacing']]
+	
+	//div[@id='fe-style-selector-field-style']/div[div[text()='label style']]
+	//div[@id='fe-style-selector-field-style']/div[div[text()='field style when unselected']]
 	
 	
 
 	public SmartForm_Edit clickSubmitButton() throws Exception {
 		Actions builder = new Actions(driver);
 		builder.moveToElement(buttonSubmit).click().build().perform();
-		Thread.sleep(10000);
+		Thread.sleep(3000);
 		return PageFactory.initElements(driver, SmartForm_Edit.class);
 	}
 
+	
+	public SmartForm_Edit clickFormStyle() throws Exception {
+		Thread.sleep(2000);
+		formStyletab.click();
+		Thread.sleep(3000);
+		return PageFactory.initElements(driver, SmartForm_Edit.class);
+	}
+
+	
 	public SmartForm_Edit editFontFamily(String option) throws Exception {
 		Actions builder = new Actions(driver);
 		WebElement selectFont = selectOption(option);
@@ -262,7 +334,7 @@ public class SmartForm_Edit extends AbstractPage {
 	}
 	
 	//=====================================================================================================================================
-	public WebElement getSelectElementBasedOnLabel (String label) throws Exception{
+	/*public WebElement getSelectElementBasedOnLabel (String label) throws Exception{
 		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
 		Iterator<WebElement> itr = selectParentElements.iterator();
 		while (itr.hasNext()) {
@@ -284,6 +356,138 @@ public class SmartForm_Edit extends AbstractPage {
 			}
 		}
 		return getSelectLblElementBasedOnLabel(label);
+		
+	}*/
+	
+	public WebElement getSelectElementBasedOnLabel (String label) throws Exception{
+		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
+		Iterator<WebElement> itr = selectParentElementsLabel.iterator();
+		while (itr.hasNext()) {
+			WebElement eachParentElement = (WebElement) itr.next();
+			Common.scrollTillElementVisisble(driver, eachParentElement);
+			try {
+				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+				String textfromApp = eachParentElement.getText().trim();
+				System.out.println (textfromApp);
+				if (textfromApp.equalsIgnoreCase(label)){
+					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::select"));
+					return selectWebElement;
+					
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		//return getSelectLblElementBasedOnLabel(label);
+		throw new Exception("label not found");
+		
+	}
+	
+	public WebElement getSelectElementBasedOnLabelDiv (List<WebElement> listElements, String label) throws Exception{
+		System.out.println("=====================getSelectElementBasedOnLabelDiv=======================");
+		System.out.println(listElements.size());
+		Iterator<WebElement> itr = listElements.iterator();
+		while (itr.hasNext()) {
+
+			WebElement eachParentElement = (WebElement) itr.next();
+			Common.scrollTillElementVisisble(driver, eachParentElement);
+			try {
+				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+				String textfromApp = eachParentElement.getText().trim();
+				System.out.println (textfromApp);
+				if (textfromApp.equalsIgnoreCase(label)){
+					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::select"));
+					return selectWebElement;
+					
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			}
+		throw new Exception ("No Label Found");
+		
+		
+	}
+	
+	
+//	public WebElement getSelectElementBasedOnLabelDivwaste (List<WebElement> listElements, String label) throws Exception{
+//		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
+//		Iterator<WebElement> itr = listElements.iterator();
+//		while (itr.hasNext()) {
+//
+//			WebElement eachParentElement = (WebElement) itr.next();
+//			Common.scrollTillElementVisisble(driver, eachParentElement);
+//			try {
+//				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+//				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+//				String textfromApp = eachParentElement.getText().trim();
+//				System.out.println (textfromApp);
+//				if (textfromApp.equalsIgnoreCase(label)){
+//					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::select"));
+//					return selectWebElement;
+//					
+//				}
+//				
+//			} catch (Exception e) {
+//				// TODO: handle exception
+//			}
+//			}
+//		throw new Exception ("No Label Found");
+//		
+//	}
+//	
+	
+	
+	public WebElement getSelectElementBasedOnLabelDiv (List<WebElement> listElementsSet1, List<WebElement> listElementsSet2, String label) throws Exception{
+		try {
+			return getSelectElementBasedOnLabelDiv (listElementsSet1, label);
+		} catch (Exception e) {
+			return getSelectElementBasedOnLabelDiv (listElementsSet2, label);
+		}
+		
+		
+	}
+	
+	
+	public WebElement getInputElementBasedOnLabelDiv (List<WebElement> listElements, String label) throws Exception{
+		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
+		Iterator<WebElement> itr = listElements.iterator();
+		while (itr.hasNext()) {
+
+			WebElement eachParentElement = (WebElement) itr.next();
+			Common.scrollTillElementVisisble(driver, eachParentElement);
+			try {
+				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+				String textfromApp = eachParentElement.getText().trim();
+				System.out.println (textfromApp);
+				if (textfromApp.equalsIgnoreCase(label)){
+					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::input"));
+					return selectWebElement;
+					
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			}
+		throw new Exception ("No Label Found");
+		
+	}
+	
+	
+	
+	public WebElement getInputElementBasedOnLabelDiv (List<WebElement> listElementsSet1, List<WebElement> listElementsSet2, String label) throws Exception{
+		try {
+			return getInputElementBasedOnLabelDiv (listElementsSet1, label);
+		} catch (Exception e) {
+			return getInputElementBasedOnLabelDiv (listElementsSet2, label);
+		}
+		
 		
 	}
 	
@@ -313,7 +517,11 @@ public class SmartForm_Edit extends AbstractPage {
 		throw new Exception ("No Label Match found");	
 	}
 	
-	public WebElement getInputTextElementBasedonLabel (String label) throws Exception {
+	
+	
+	
+	
+	/*public WebElement getInputTextElementBasedonLabel (String label) throws Exception {
 		
 		System.out.println("=====================getTextBoxOnTheLabelisDiv=======================");
 		Iterator<WebElement> itr = txtFieldParentElements.iterator();
@@ -359,11 +567,68 @@ public class SmartForm_Edit extends AbstractPage {
 		
 		throw new Exception ("No Label Match found");
 		//return null;
+	}*/
+	
+	
+	public WebElement getInputTextDIVElementBasedonLabel (String label) throws Exception{
+		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
+		
+		
+		Iterator<WebElement> itr = inputtParentElementsDiv.iterator();
+		while (itr.hasNext()) {
+			WebElement eachParentElement = (WebElement) itr.next();
+			Common.scrollTillElementVisisble(driver, eachParentElement);
+			try {
+				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+				String textfromApp = eachParentElement.getText().trim();
+				System.out.println (textfromApp);
+				if (textfromApp.equalsIgnoreCase(label)){
+					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::input"));
+					return selectWebElement;
+					
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		//return getSelectLblElementBasedOnLabel(label);
+		throw new Exception("label not found");
+		
+	}
+	
+	public WebElement getInputTextElementBasedonLabel (String label) throws Exception{
+		System.out.println("=====================setValueOfSelectBoxBasedOnLabel=======================");
+		Iterator<WebElement> itr = inputParentElementsLabel.iterator();
+		while (itr.hasNext()) {
+
+			WebElement eachParentElement = (WebElement) itr.next();
+			Common.scrollTillElementVisisble(driver, eachParentElement);
+			try {
+				//WebElement lblElement = eachParentElement.findElement(By.xpath("./label"));
+				//WebElement lblElement = eachParentElement.findElement(By.xpath(".."));
+				String textfromApp = eachParentElement.getText().trim();
+				System.out.println (textfromApp);
+				if (textfromApp.equalsIgnoreCase(label)){
+					WebElement selectWebElement = eachParentElement.findElement(By.xpath("./following-sibling::input"));
+					return selectWebElement;
+					
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			}
+		return getInputTextDIVElementBasedonLabel(label);
+		
 	}
 	
 	
+	
 	public SmartForm_Edit setTextInFieldSettingsBasedOnLabel (String label, String value) throws Exception{
-		WebElement inputTextElement = getInputTextElementBasedonLabel(label);
+		//WebElement inputTextElement = getInputTextElementBasedonLabel(label);
+		WebElement inputTextElement = getInputElementBasedOnLabelDiv(inputParentElementsLabel,inputtParentElementsDiv,label);
 		inputTextElement.clear();
 		inputTextElement.sendKeys(value);
 		return PageFactory.initElements(driver, SmartForm_Edit.class);
@@ -371,21 +636,91 @@ public class SmartForm_Edit extends AbstractPage {
 	
 	
 	public SmartForm_Edit setSelectInFieldSettingsBasedOnLabel (String label, String value) throws Exception{
-		WebElement selectElement = getSelectElementBasedOnLabel(label);
+		//WebElement selectElement = getSelectElementBasedOnLabel(label);
+		WebElement selectElement = getSelectElementBasedOnLabelDiv(selectParentElementsDiv,selectParentElementsLabel,label);
 		Common.selecteValueFromDropDown(driver, selectElement, value);
 		return PageFactory.initElements(driver, SmartForm_Edit.class);
 	}
 	
 	
 	public boolean verifyTextInFieldSettingsBasedOnLabel (String label, String expectedValue) throws Exception{
-		WebElement inputTextElement = getInputTextElementBasedonLabel(label);
+		WebElement inputTextElement = getInputElementBasedOnLabelDiv(inputParentElementsLabel,inputtParentElementsDiv,label);
 		return Common.verifyTextBoxValue(inputTextElement, expectedValue);	
 	}
 	
 	
 	public boolean verifySelectedOptionInFieldSettingsBasedOnLabel (String label, String expectedValue) throws Exception{
-		WebElement selectElement = getSelectElementBasedOnLabel(label);
+		//WebElement selectElement = getSelectElementBasedOnLabel(label);
+		//WebElement selectElement = getSelectElementBasedOnLabelDiv(label);
+		WebElement selectElement = getSelectElementBasedOnLabelDiv(selectParentElementsDiv,selectParentElementsLabel,label);
 		return Common.verifySelectDropDownValue(selectElement, expectedValue);	
 	}
 
+	/* Form Style methods */
+	
+	public SmartForm_Edit setTextInFormStyleBasedOnLabel (String sectionName,String label, String value) throws Exception{
+		//WebElement inputTextElement = getInputTextElementBasedonLabel(label);
+		//WebElement inputTextElement = getInputElementBasedOnLabelDiv(inputFormStylParentElementsLabel,inputFormStylParentElementsDiv,label);
+		WebElement inputTextElement = null;
+		if (sectionName.equalsIgnoreCase("label style")){
+			//div[@id='fe-style-selector-field-style']//div[div[text()='label style']]
+			inputTextElement = getInputElementBasedOnLabelDiv(inputFormStylLabelStyleSelectDiv,label);
+		}
+		else if (sectionName.equalsIgnoreCase("Input Text Style")){
+			inputTextElement = getInputElementBasedOnLabelDiv(selectFormStylInputTextStylSelectDiv,label);
+		}else if (sectionName.equalsIgnoreCase("sub text style")){
+		inputTextElement = getInputElementBasedOnLabelDiv(inputFormStylSubTextStyleSelectDiv,label);
+		}
+		
+		inputTextElement.clear();
+		inputTextElement.sendKeys(value);
+		return PageFactory.initElements(driver, SmartForm_Edit.class);
+	}
+	
+	
+	public SmartForm_Edit setSelectInFormStyleBasedOnLabel (String sectionName, String label, String value) throws Exception{
+		//WebElement selectElement = getSelectElementBasedOnLabel(label);
+		//WebElement selectElement = getSelectElementBasedOnLabelDiv(selectFormStylParentElementsDiv,selectFormStylParentElementsLabel,label);
+		WebElement selectElement = null;
+		
+		if (sectionName.equalsIgnoreCase("label style")){
+			//div[@id='fe-style-selector-field-style']//div[div[text()='label style']]
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylLabelStyleSelectDiv,label);
+		}
+		else if (sectionName.equalsIgnoreCase("Input Text Style")){
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylInputTextStylSelectDiv,label);
+		}else if (sectionName.equalsIgnoreCase("sub text style")){
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylSubTextStyleSelectDiv,label);
+		}
+		
+		Common.selecteValueFromDropDown(driver, selectElement, value);
+		return PageFactory.initElements(driver, SmartForm_Edit.class);
+	}
+	
+	
+	public boolean verifyTextInFormStyleBasedOnLabel (String label, String expectedValue) throws Exception{
+		WebElement inputTextElement = getInputElementBasedOnLabelDiv(inputFormStylParentElementsLabel,inputFormStylParentElementsDiv,label);
+		return Common.verifyTextBoxValue(inputTextElement, expectedValue);	
+	}
+	
+	
+	public boolean verifySelectedOptionInFormStyleBasedOnLabel (String sectionName, String label, String expectedValue) throws Exception{
+		//WebElement selectElement = getSelectElementBasedOnLabel(label);
+		//WebElement selectElement = getSelectElementBasedOnLabelDiv(label);
+		WebElement selectElement = null;
+		if (sectionName.equalsIgnoreCase("label style")){
+			//div[@id='fe-style-selector-field-style']//div[div[text()='label style']]
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylLabelStyleSelectDiv,label);
+		}
+		else if (sectionName.equalsIgnoreCase("Input Text Style")){
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylInputTextStylSelectDiv,label);
+		}else if (sectionName.equalsIgnoreCase("sub text style")){
+			selectElement = getSelectElementBasedOnLabelDiv(selectFormStylSubTextStyleSelectDiv,label);
+		}
+	//	selectElement = getSelectElementBasedOnLabelDiv(selectFormStylParentElementsDiv,selectFormStylParentElementsLabel,label);
+		
+		return Common.verifySelectDropDownValue(selectElement, expectedValue);	
+	}
+
+	
 }
